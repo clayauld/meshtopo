7dd67af0bb5eccc10454a530c3794b603d14b9a4
Clayton Auld <clayton@clayauld.com>
2026-03-20 20:01:57 -0800

    Fix mypy flagging error in github actions

--- src/web/views.py
+++ src/web/views.py
@@ -1,5 +1,7 @@
 """View handlers for the Web UI."""
 
+# mypy: disable-error-code="untyped-decorator"
+
 import logging
 import os
 from typing import Any, Dict
@@ -22,7 +24,7 @@ async def index(request: web.Request) -> web.Response:
         raise web.HTTPFound("/login")
 
 
-@aiohttp_jinja2.template("login.html")  # type: ignore[untyped-decorator]
+@aiohttp_jinja2.template("login.html")
 async def login_get(request: web.Request) -> Dict[str, Any]:
     """Handle GET requests for the login page."""
     session = await get_session(request)
@@ -31,7 +33,7 @@ async def login_get(request: web.Request) -> Dict[str, Any]:
     return {"error": "", "csrf_token": await generate_csrf(request)}
 
 
-@aiohttp_jinja2.template("login.html")  # type: ignore[untyped-decorator]
+@aiohttp_jinja2.template("login.html")
 async def login_post(request: web.Request) -> Dict[str, Any]:
     """Handle POST requests for the login page."""
     data = await request.post()
@@ -83,7 +85,7 @@ async def logout(request: web.Request) -> web.Response:
 
 
 @login_required
-@aiohttp_jinja2.template("config.html")  # type: ignore[untyped-decorator]
+@aiohttp_jinja2.template("config.html")
 async def config_get(request: web.Request) -> Dict[str, Any]:
     """Handle GET requests for the config dashboard."""
     gateway_app = request.app[GATEWAY_APP_KEY]
@@ -226,7 +228,7 @@ async def api_logs_get(request: web.Request) -> web.Response:
 
 
 @login_required
-@aiohttp_jinja2.template("status.html")  # type: ignore[untyped-decorator]
+@aiohttp_jinja2.template("status.html")
 async def status_get(request: web.Request) -> Dict[str, Any]:
     """Handle GET requests for the status dashboard."""
     import collections
